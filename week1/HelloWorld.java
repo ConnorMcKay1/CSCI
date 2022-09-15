daeaa86a8783ea986f2929d2f102812af4c2badf
Connor <mckayc1@go.stockton.edu>
2022-09-15 19:17:36 -0400

    it's a test

---
+++ week1/HelloWorld.java
@@ -0,0 +1,18 @@
+package week1;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+
+
+        System.out.println("Hello WOrld!");
+    
+        System.out.println("hey");
+
+        int a = 5;
+        int b = 10;
+
+        System.out.println(a+b);
+
+    
+    }
+}
