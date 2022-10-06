e9b2d97753ec45172f6e67bc8c69580975318e6f
Connor <mckayc1@go.stockton.edu>
2022-10-05 21:27:24 -0400

    brainstorming

--- week1/HelloWorld.java
+++ week1/HelloWorld.java
@@ -7,6 +7,85 @@ public class HelloWorld {
     
     public static void main(String[] args) {
 
+
+
+        // Write some code here to print out the sum of 2 and 3 to the console.
+
+
+        Machines x1a = new Machines(x: 1);
+        
+        Machines y1a = new Machines(y: 2);
+
+
+        System.out.println(x1a.x);
+
+
+
+
+        //Write some code here to call the Output method from the Pyramid class.
+        // Note: Since Pyramid is a static method you will need to call it like this:
+        // Class.Method()
+        // Note2: Look at Pyramid.java for more information
+
+
+
+        
+
+
+
+
+        
+
+
+/*-----------------------------------------------------------------------------------------------------
+        PROBLEM 1
+
+ * 
+ * put the addition method (machine) in another class (tab)
+ * 
+ * 
+ * -look at the most recent zoom lecture to see how it can be referenced in the main method
+ *      -theres gonna be something about trying to reference them
+ *      -and in the main method, that is where the inputs for the numbers (variables) is gonna be
+ * 
+ * 
+ * 
+ * ------------------------------------------------------------------------------------------------------
+ *      PROBLEM 2
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        /* 
     Rectangle box = new Rectangle(5, 10, 20, 30);
 
     System.out.println(box);
---
+++ week1/Machines.java
@@ -0,0 +1,26 @@
+package week1;
+
+public class Machines.java {
+
+
+
+public int x = x1;
+public int y = y1;
+
+
+
+
+public Machines(int x) {
+
+this.x = x;
+
+}
+
+public Machines(int y) {
+
+this.y = y;
+
+}
+
+
+}
