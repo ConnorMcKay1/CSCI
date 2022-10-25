d9ffaacd39bf52a2ae9a68ee453acd6ecfda0d22
Connor <mckayc1@go.stockton.edu>
2022-10-24 21:18:57 -0400

    some more homework

--- week1/week1/Machines.java
+++ week1/week1/Machines.java
@@ -1,21 +1,48 @@
 package week1;
 
-import javax.print.attribute.standard.RequestingUserName;
-
 public class Machines {
 
 
+String p1 = "#";
+String p2 = "##";
+String p3 = "###";
+String p4 = "####";
+String p5 = "#####";
+String p6 = "####";
+String p7 = "###";
+String p8 = "##";
+String p9 = "#";
 
-public static String adlibs(String word1, String word2, int number) {
 
-        String answer = "Hello, my name is " + word1 + " and I like to " + word2 + " because I'm only " + number + " years old.";
-        return answer;
+public static String pyramid(String pyramid) {
+
+static System.out.println(p1);
+static System.out.println(p2);
+System.out.println(p3);
+System.out.println(p4);
+System.out.println(p5);
+System.out.println(p6);
+System.out.println(p7);
+System.out.println(p8);
+System.out.println(p9);
+
+}
+
+        
+
+
+
+
+
+
+
+
 }
 
 
 
+
+
+
 }
 
-
-//    String answer = System.out.println("Hello, my name is " + word1 + " and I like to " + word2 + " because I'm only " + number + " years old.");
-// return 
