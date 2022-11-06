1ffe19b1811f4276a885fd25cd3f95e667bfa426
Connor <mckayc1@go.stockton.edu>
2022-11-06 15:09:26 -0500

    finished

--- week1/week1/Machines.java
+++ week1/week1/Machines.java
@@ -1,16 +1,35 @@
 package week1;
 
+import javax.swing.event.SwingPropertyChangeSupport;
+
 public class Machines {
 
 
 
+String name = "Jon";
 
+public String getName() {
+
+return name;
 
 }
 
 
+public int nameLength(String name) {
+   int nameLength = name.length();
+
+    return nameLength;
+
+}
+
+public String nameUpperCase(String name) {
+    String nameUpperCase = name.toUpperCase();
+
+    return nameUpperCase;
+}
 
 
+}
 
 
 
